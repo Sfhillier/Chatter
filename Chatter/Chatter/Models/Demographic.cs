90092604a6bccc3d3b11f49e473494c4ae4cc141
Scott Hillier <Sfhillier@gmail.com>
2015-11-01 11:47:04 -0500

    More classes have been added. Close to being ready to scaffold.

---
+++ Chatter/Chatter/Models/Demographic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chatter.Models
+{
+    public class Demographic
+    {
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public DateTime BirthDate { get; set; }
+        public char Gender { get; set; }
+
+    }
+}
