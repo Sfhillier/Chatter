93f4dd75576dee44c14b9852c9c83fa21165404e
Scott Hillier <sfhillier@gmail.com>
2015-11-06 11:32:12 -0500

    Added the rest of the models
    Some data annotations and the remaining models / properties have been created. Almost ready for scaffolding

---
+++ Chatter/Chatter/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Chatter.Models
+{
+    public class Message
+    {
+        [Required]
+        public Member Recipient { get; set; }
+        public string MessageText { get; set; }
+        public DateTime DateSent { get; set; }
+        public bool HasRead { get; set; }
+        public MessageID ThreadID { get; set; }
+    }
+}
---
+++ Chatter/Chatter/Models/Photo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chatter.Models
+{
+    public class Photo
+    {
+        public string Path { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        public bool ProfilePicture { get; set; }
+    }
+}
